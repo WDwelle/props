14f2cb59d90bf900ae54d779a6474214a8c13e95
WDwelle <willdwelle@frontier.com>
2022-01-11 17:10:27 -0800

    switched from class to functional components

--- src/components/People.js
+++ src/components/People.js
@@ -1,29 +1,29 @@
 import React, {Component} from 'react';
 
-class People extends Component {
+const People = props => {
 
-    constructor (props){
-        super(props);
-        this.state = {
-            age: this.props.age
-        };
-    }
+    //constructor (props){
+        //super(props);
+        //this.state = {
+            //age: this.props.age
+        //};
+    //}
 
-    increaseAge = () => {
-        this.setState({age: this.state.age + 1})
-    }
-    render() {
+    //increaseAge = () => {
+        //this.setState({age: this.state.age + 1})
+    //}
+    //render() {
         return (
             <div>
                 <ul>
-                    <li><h1>{ this.props.lastName } {this.props.firstName }</h1></li>
-                    <li>Age: { this.state.age}</li>
-                    <li>Hair color: { this.props.hairColor}</li>
-                    <button onClick={ this.increaseAge }>Birthday</button>
+                    <li><h1>{ props.lastName } {props.firstName }</h1></li>
+                    <li>Age: { props.age}</li>
+                    <li>Hair color: { props.hairColor}</li>
+
                 </ul>
             </div>
         );
-    }
+    //}
 }
 
 export default People;
